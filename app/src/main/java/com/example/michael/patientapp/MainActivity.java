3c9ec6895896983088420cf5793b0d48cffdf4f6
m-hou <michaeldamush@gmail.com>
2017-04-29 19:36:28 -0700

    added listview and new activity on selection

--- app/src/main/java/com/example/michael/patientapp/MainActivity.java
+++ app/src/main/java/com/example/michael/patientapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.michael.patientapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                Intent intent = new Intent(MainActivity.this, PatientActivity.class);
+                intent.putExtra("title", mobileArray[position]);
+                startActivity(intent);
+            }
+        });
     }
 }
